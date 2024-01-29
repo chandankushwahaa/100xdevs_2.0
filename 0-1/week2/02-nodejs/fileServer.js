06d0622ad66e0c65df04808e558f5b51a7f4ba22
chandan <chandan7979ck@gmail.com>
2024-01-29 20:20:19 +0530

    solved assigment week-2

--- 0-1/week2/02-nodejs/fileServer.js
+++ 0-1/week2/02-nodejs/fileServer.js
@@ -12,46 +12,10 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
-const dir = path.join(__dirname, "./files");
-
-// Endpoint to get the list of files
-app.get("/files", (req, res) => {
-  // Read directory asynchronously
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      // If there's an error reading the directory, return 500 Internal Server Error
-      return res.status(500).send("Internal Server Error");
-    }
-    // If successful, return the list of files as JSON
-    res.status(200).json(files);
-  });
-});
-
-// Endpoint to get content of a specific file by name
-app.get("/file/:filename", function (req, res) {
-  const { filename } = req.params;
-  const filepath = path.join(dir, filename); // Construct full file path
-
-  // Read file asynchronously
-  fs.readFile(filepath, "utf8", (err, data) => {
-    if (err) {
-      // If file is not found, return 404 File Not Found
-      return res.status(404).send("File not found");
-    }
-    // If file is found, return its content
-    res.send(data);
-  });
-});
-
-// Handling undefined routes with a 404 Not Found
-app.use((req, res) => {
-  res.status(404).send("Route not found");
-});
 
 module.exports = app;
