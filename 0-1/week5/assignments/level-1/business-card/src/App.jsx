d159a52fe43ba9ad6cf17c847f4a2e7f32a9cfab
chandan <chandan7979ck@gmail.com>
2024-02-15 12:10:29 +0530

    week 5 assignments

---
+++ 0-1/week5/assignments/level-1/business-card/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className='container'>
+        <h1>Chandan Kushwaha</h1>
+        <p>A TA in the 100xDevs Chohort 2.0</p>
+
+        <div>
+          <h2>Interests</h2>
+          <p>DSA</p>
+          <p>Open Source</p>
+          <p>Full Stack Develpoment</p>
+        </div>
+
+        <div>
+          <button>LinkedIn</button>
+          <button>Twitter</button>
+        </div>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App;
